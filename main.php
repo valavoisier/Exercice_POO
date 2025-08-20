27feaf5cdfe5ec94e572f278b213318a3523a8c0
Valerie Lavoisier <val.lavoisier3@gmail.com>
2025-08-20 16:35:15 +0200

    test list

--- main.php
+++ main.php
@@ -2,4 +2,7 @@
 while (true) {
     $line = readline("Entrez votre commande : ");
     echo "Vous avez saisi : $line\n";
+    if ($line === "list") {
+        echo "SELECT * FROM contact \n";
+    }
 }
